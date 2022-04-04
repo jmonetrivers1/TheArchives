4ed6d76f30ad1e1b0739c86ed3f65c60ecff3b1a
JeMarra (Chrome) <jmonetrivers1@gmail.com>
2022-04-04 15:12:44 -0500

    Just Some Demo Stuff

---
+++ Assets/Scripts/CampaignPieces/NPC.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPC : MonoBehaviour
+{
+    public string name = "";
+    public string estimatedClass = "";
+    public string location = "";
+    public string race = "";
+    public string occupation = "";
+    public string notes ="";
+    public string partyAlignment = "";
+    public int status = 3;
+        //0 -Dead
+        //1-Alive
+        //2-Unknown
+
+
+}
---
+++ Assets/Scripts/CampaignPieces/PC.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PC : MonoBehaviour
+{
+    public string characterName = "";
+    public string player = "";
+    public string primaryClass =  "";
+    public string totalLevel ="";
+    public int status = 3;
+
+    public string importantNotes = "";
+    public string generalNotes = "";
+}
