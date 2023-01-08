def70f507acd88586dfab17b6229ad16b0568ec3
JeMarra Rivers <jmonetrivers1@gmail.com>
2023-01-08 15:36:41 -0600

    Ui Memes and Damage Calculator Added

--- Assets/Scripts/Tools/DamageCalulator.cs
+++ Assets/Scripts/Tools/DamageCalulator.cs
@@ -1,18 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class DamageCalulator : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    //damage decimals/fractions rounded down
+    public TMP_InputField originalDamage;
+    public TextMeshProUGUI halfDamage;
+    public TextMeshProUGUI quarterDamage;
+    public TextMeshProUGUI eigthDamage;
+    public TextMeshProUGUI doubleDamage;
+    public void SpellSaveDamage()
     {
-        
+        if (int.Parse(originalDamage.text) > 0) //passes check int, positive
+        {
+            int ogInt = int.Parse(originalDamage.text);
+            //.ToString(); BELOW ROUNDS DOWN
+            halfDamage.text = (ogInt/2).ToString(); 
+            quarterDamage.text = (ogInt / 4).ToString();
+            eigthDamage.text = (ogInt / 8).ToString();
+            doubleDamage.text = (ogInt * 2).ToString();
+            return;
+        }
+        else if (int.Parse(originalDamage.text) == 0) //equals zero
+        {
+            originalDamage.text = "0";
+            halfDamage.text = "0"; 
+            quarterDamage.text = "0"; 
+            eigthDamage.text = "0"; 
+            doubleDamage.text = "0"; 
+        }
+        else //invalid number entered
+        {
+            originalDamage.text = "0";
+            halfDamage.text = "x"; //removes number/texts and puts an x in box
+            quarterDamage.text = "x"; 
+            eigthDamage.text = "x";
+            doubleDamage.text = "x"; 
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
--- Assets/Scripts/Tools/Hypothesus.cs
+++ Assets/Scripts/Tools/Hypothesus.cs
@@ -1,18 +1,59 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
+//a script for calculating the hypothenus
+//a2 + b2 = c2
+//c = sq(a2 + b2
 public class Hypothesus : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public TMP_InputField aVariable;
+    public TMP_InputField bVariable;
+    public TextMeshProUGUI cVariable;
+    //check a Variable (positive, less than 10,000?)
+    
+    public void a_VariableCheck()
     {
-        
+        if (aVariable.text == "" ||int.Parse(aVariable.text) <=0|| int.Parse(aVariable.text) > 10000) //if the number is out of bounds / les than 0 or greater than 10,000
+        {
+            cVariable.text = "-"; //removes number/texts and puts a dash in hypothenus
+            aVariable.text = "0";
+            return;
+        }
+        else if (bVariable.text == "" || int.Parse(bVariable.text) <= 0 || int.Parse(bVariable.text) > 10000) //check bvariable
+        {
+            cVariable.text = "-"; //removes number/texts and puts a dash in hypothenus
+            bVariable.text = "0";
+            return;
+        }
+        else //all clear (i hope so lol?)
+        {
+            float cNum = Mathf.Sqrt(int.Parse(aVariable.text) * int.Parse(aVariable.text) + int.Parse(bVariable.text) * int.Parse(bVariable.text));
+            cNum = Mathf.Round(cNum * 100) / 100;
+            cVariable.text = cNum.ToString(); return;
+        }
     }
-
-    // Update is called once per frame
-    void Update()
+    public void b_VariableCheck()
     {
-        
+        if (bVariable.text == "" || int.Parse(bVariable.text) <= 0 || int.Parse(bVariable.text) > 10000) //check bvariable
+        {
+            cVariable.text = "-"; //removes number/texts and puts a dash in hypothenus
+            bVariable.text = "0";
+            return;
+        }
+        else if (aVariable.text == "" || int.Parse(aVariable.text) <= 0 || int.Parse(aVariable.text) > 10000) //if the number is out of bounds / les than 0 or greater than 10,000
+        {
+            cVariable.text = "-"; //removes number/texts and puts a dash in hypothenus
+            aVariable.text = "0";
+            return;
+        }
+        else //all clear (i hope so lol?)
+        {
+            float cNum = Mathf.Sqrt(int.Parse(aVariable.text) * int.Parse(aVariable.text) + int.Parse(bVariable.text) * int.Parse(bVariable.text));
+            cNum = Mathf.Round(cNum * 100) / 100;
+            cVariable.text = cNum.ToString();
+            return;
+        }
     }
 }
